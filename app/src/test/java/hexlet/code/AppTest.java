92da0cafc11967fd9b90f1933925eea166edb06a
Sergei <mre@mail.ru>
2023-02-10 16:58:23 +0800

    AppTest

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,38 @@
+package hexlet.code;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AppTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(output));
+    }
+
+    @Test
+    @DisplayName("'main' method works correctly")
+    void testMain() {
+        log.debug("Start test");
+        App.main(null);
+        assertEquals("Hello, World!", output.toString(StandardCharsets.UTF_8).trim());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+}
