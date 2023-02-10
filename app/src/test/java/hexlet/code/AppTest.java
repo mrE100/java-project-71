393d1a738473d31d2f43becbc7f872fc4d10060c
Sergei <mre@mail.ru>
2023-02-10 18:27:27 +0800

    test correction

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -27,8 +27,15 @@ public class AppTest {
     @DisplayName("'main' method works correctly")
     void testMain() {
         log.debug("Start test");
-        App.main(null);
-        assertEquals("Hello, World!", output.toString(StandardCharsets.UTF_8).trim());
+        App.main(new String[]{"filepath1.json filepath2.json"});
+        assertEquals("{\n" +
+                "  - follow: false\n" +
+                "    host: hexlet.io\n" +
+                "  - proxy: 123.234.53.22\n" +
+                "  - timeout: 50\n" +
+                "  + timeout: 20\n" +
+                "  + verbose: true\n" +
+                "}", output.toString(StandardCharsets.UTF_8).trim());
     }
 
     @AfterEach
